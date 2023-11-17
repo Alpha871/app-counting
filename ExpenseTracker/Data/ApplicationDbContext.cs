a0690a2c2e03a585bce83d151e4da4066f7e4c24
Alpha Bah <alphabah@MacBook-Pro-de-Alpha.local>
2023-11-17 16:19:13 +0300

    sync

---
+++ ExpenseTracker/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ExpenseTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.Data
+{
+	public class ApplicationDbContext:DbContext
+	{
+		public ApplicationDbContext(DbContextOptions options) : base(options)
+		{
+		}
+
+		public DbSet<Category> Categories { get; set; }
+		public DbSet<Transaction> Transactions { get; set; }
+    }
+}
+
---
+++ ExpenseTracker/Models/Transaction.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Globalization;
+
+namespace ExpenseTracker.Models
+{
+	public class Transaction
+	{
+		[Key]
+		public int TransactionId { get; set; }
+
+		[Range(1,int.MaxValue, ErrorMessage ="Please select a category.")]
+		public int CategoryId { get; set; }
+		public Category? Category { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Amount should be greater than 0.")]
+        public int Amount { get; set; }
+		public string? Note { get; set; }
+		public DateTime Date { get; set; } = DateTime.Now;
+
+		[NotMapped]
+		public string? categoryTitleWithIcon
+		{
+			get
+			{
+				return Category == null ? "" : Category.Icon + " " + Category.Title;
+			}
+		}
+
+        [NotMapped]
+        public string? FormattedAmount
+        {
+ 
+
+            get
+            {
+                CultureInfo culture = CultureInfo.CreateSpecificCulture("en-US");
+                culture.NumberFormat.CurrencyNegativePattern = 1;
+	
+				return ((Category == null || Category.Type == "Expense") ? "-" : "+") + String.Format(culture, "{0:C0}", Amount);
+            }
+        }
+    }
+}
+
